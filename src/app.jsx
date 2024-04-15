c4695483d9e1558abdf70388f8130315a180511e
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-04-15 12:38:58 +0530

    feat: add app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-pink-500 flex items-center justify-center h-screen text-2xl bg-gray-900">
+      Hello
+    </h1>
+  );
+}
+
+export default App;
