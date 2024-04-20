acc05741b40a844c2e09c305cfdfac8e1e11334d
rushi-mungse <mungse.rushi@gmail.com>
2024-04-20 21:18:03 +0530

    feat: text

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <h1 className="text-pink-500 flex items-center justify-center h-screen text-2xl bg-gray-900">
-      Hello Welcome to Code Mitra YT
+      Hello Welcome to Code Mitra YT Youtube Channel
     </h1>
   );
 }
